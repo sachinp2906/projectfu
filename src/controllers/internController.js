b58ecb93f5a38647e260dde9c100d1fe8234ca76
abhinav822 <pabhinav400@gmail.com>
2022-11-21 21:46:42 +0530

    intern_controller

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -6,22 +6,68 @@ const {isValidBody,isValidEmail,isValidMobile,isValid,isValidintern} = require("
 
 // create intern data
 
-const createIntern = async(req,res) => {
-
-    const data = req.body
-    let {name,email,mobile,collegeName} = data
-
-    if (!isValidBody(data)){
-        return res.status(400).send({status:false, message:"Interns details required"})
-
+exports. createIntern = async (req, res) => {  // this type of exporting is called named exporting
+    // res.setHeader('Access-Conrol-Allow-Origin', '*');   // to allow cross origin resource sharing
+    try {
+      //res.setHeader("Access-Control-Allow-Origin", "*");
+      const data = req.body;
+      let { name, email, mobile, collegeName } = data;
+  
+      if (!isValidBody(data)) {
+        return res.status(400).send({ status: false, message: "Interns details are required." });
+      }
+  
+      if (!name)
+        return res.status(400).send({ status: false, message: "Please Enter Your Name" });
+      if (!isValidintern(name))
+        return res.status(400).send({ status: false, message: "Please Enter Valid Name" });
+  
+      if (!email)
+        return res.status(400).send({ status: false, msg: "Please Enter your Email Id" });
+      if (!isValidEmail(email))
+        return res.status(400).send({ status: false, msg: "Please Enter a valid Email Id." });
+  
+      let emailExited = await internModel.findOne({ email: email });
+      if (emailExited)
+        return res.status(400).send({status: false,msg: "This Email already existed, Please Try another !"});
+  
+      if (!mobile)
+        return res.status(400).send({ status: false, message: "Please Enter Your Mobile Number" });
+      if (!isValidMobile(mobile.trim()))
+        return res.status(400).send({status: false,message: "Mobile no. should contain only 10 digits"});
+  
+      let existedMobile = await internModel.findOne({ mobile });
+      if (existedMobile)
+        return res.status(400).send({status: false,message: "This Mobile No. is already registered"});
+  
+      if (!collegeName)
+        return res.status(400).send({ status: false, message: "Please Enter College Name" });
+      if (!isValid(collegeName))
+        return res.status(400).send({ status: false, message: "Please Enter Valid CollegeName" });
+  
+      let collegeData = await collegeModel.findOne({ name: collegeName });
+      if (!collegeData)
+        return res.status(404).send({ status: false, message: "No Such College Found" });
+  
+      data.collegeId = collegeData._id.toString();// this line means that we are adding a new property to the data object and assigning it the value of collegeData._id.toString()
+  
+      let internData = await internModel.create(data);
+      let newIntern = {
+        name: internData.name,
+        email: internData.email,
+        mobile: internData.mobile,
+        collegeId: internData.collegeId,
+        isDeleted: internData.isDeleted,
+      };
+  
+      return res.status(201).send({ status: true, data: newIntern });
+    } catch (err) {
+      res.status(500).send({ status: false, message: err.message });
     }
-    if (!name)
-        return res.status(400).send({status:false, message:"Please enter your name"})
-    if(!isValidintern(name))
-        return res.status(400).send({status:false,message:"Please enter valid name"})
+  };
+  
+  //<<======================= Imported Module =================================>>//
+  
+  // module.exports = { createIntern };  // this type of exporting is called default exporting
 
-    if(!email)
-        return res.status(400).send({status:false,msg:"please enter email id"})
-    if(!isValidEmail(email))
-        return res.status(400).send({status:false, msg:"enter valid email id"})
-}
+  // cors means cross origin resource sharing it is used to allow cross origin resource sharing means to allow the data to be shared between different domains like localhost:3000 and localhost:5000
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const collegeController = require("../controllers/collegeController")
-// const internController = require("../controllers/internController")
+const internController = require("../controllers/internController")
 
 
 // create college api
 
 router.post("/functionup/colleges",collegeController.createCollege)
-
-
+//--- Create Intern Api -----------
+router.post("/functionup/interns", internController.createIntern);
 
 
 
