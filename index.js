943f51e35f49862dc215c2d49efea2b5b29353bd
iqra3786 <iqra3786@gmail.com>
2022-11-21 12:27:58 +0530

    packageInstall

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const routes = require('./routes/route')
+app.use(express.json())
+mongoose.connect('mongodb+srv://Iqra:Iqra3786@cluster0.tsgwccn.mongodb.net/group29Database', {
+    useNewUrlParser: true
+})
+    .then(() => console.log("mongodb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', routes)
+app.listen((process.env.PORT || 3000), function () {
+    console.log("running on port")
+})
+
+
---
+++ models/collegeModel.js
@@ -0,0 +1,3 @@
+const mongoose=require('mongoose')
+// { name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false}
+
